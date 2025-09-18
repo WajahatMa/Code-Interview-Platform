a8ab1dc8f7437993eba37102557cbcc03e861b72
Wajahat Mahmood <waj@Wajahats-MacBook-Pro-2.local>
2025-09-18 17:48:00 -0400

    Changes to front and back end

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources = {r"/*": {"origins": "http://localhost:5173"}})
 
 
-socketio = SocketIO(app, cors_allowed_origins="*")  # allow our frontend origin
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 @app.get("/")
 
